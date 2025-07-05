5c4c3973d93e90379a6886fb3a856f49a16fd95f
prommZ <sonukumarrani851111@gmail.com>
2025-07-06 00:31:44 +0530

    Uploading my portfolio into github

---
+++ src/sections/Experiences.jsx
@@ -0,0 +1,11 @@
+import { Timeline } from "../components/Timeline";
+import { experiences } from "../constants";
+const Experiences = () => {
+  return (
+    <div className="w-full" id="experienc">
+      <Timeline data={experiences} />
+    </div>
+  );
+};
+
+export default Experiences;
