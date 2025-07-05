472c2466afbec005e7d479e75341e0a142655055
prommZ <sonukumarrani851111@gmail.com>
2025-07-06 01:06:17 +0530

    update env file

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -30,16 +30,16 @@ const Contact = () => {
     try {
       console.log("From submitted:", formData);
       await emailjs.send(
-        "service_79b0nyj",
-        "template_17us8im",
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
         {
           from_name: formData.name,
-          to_name: "Ali",
+          to_name: "Sonu",
           from_email: formData.email,
-          to_email: "AliSanatiDev@gmail.com",
+          to_email: "sonukumarrani851111@gmail.com",
           message: formData.message,
         },
-        "pn-Bw_mS1_QQdofuV"
+        process.env.REACT_APP_PUBLIC_KEY
       );
       setIsLoading(false);
       setFormData({ name: "", email: "", message: "" });
